eb10b83e5688b805f3501e7b8c9fef3a5bcc1c9b
Kostiantyn Doichev <doichev.kostia@gmail.com>
2023-09-05 08:06:02 +0200

    feat: metrics

---
+++ src/plugins/metrics.ts
@@ -0,0 +1,36 @@
+import Fastify from "fastify";
+import fp from "fastify-plugin";
+import fastifyMetrics, { type IMetricsPluginOptions } from "fastify-metrics";
+
+export default fp(async function metrics(fastify, options) {
+	fastify.register(fastifyMetrics as any, {
+		defaultMetrics: { enabled: true },
+		endpoint: null,
+		name: "metrics",
+		routeMetrics: { enabled: true },
+		clearRegisterOnInit: false,
+	} satisfies IMetricsPluginOptions);
+
+	const promServer = Fastify({
+		logger: fastify.log,
+	});
+
+	promServer.route({
+		method: "GET",
+		url: "/metrics",
+		logLevel: "info",
+		handler: async function metricsHandler(request, reply) {
+			reply.type("text/plain");
+			return fastify.metrics.client.register.metrics();
+		},
+	});
+
+	fastify.addHook("onClose", async (instance) => {
+		await promServer.close();
+	});
+
+	await promServer.listen({
+		host: "0.0.0.0",
+		port: 9001,
+	});
+}, { name: "prom" });
